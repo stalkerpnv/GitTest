07b9ebd92e39a5c1cc62ce1d89f599ac9e3fef0f
stalker_pnv <stalker_pnv@mail.ru>
2022-02-28 12:16:49 +0900

    Изменен вывод строки world

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,6 @@
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("World");
+        System.out.println("W o r l d");
     }
 }
